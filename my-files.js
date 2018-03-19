4e43934f70373a4e39b77cb87a4ba62ce0581ae8
Kelly L <lock2846@gmail.com>
2018-03-19 14:32:52 -0600

    deleted text file!

--- my-files.js
+++ my-files.js
@@ -13,14 +13,31 @@ const filename = path.join(__dirname, 'test.txt')
 //   }
 // })
 // console.log(cyan('herro'));
-fs.readFile(filename, 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
+// fs.readFile(filename, 'utf8', (err, data) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+//
+//   const lines = data
+//   .split('\n')
+//   .filter(x => x.length > 0)
+//   .map((el, idx) => `${idx}: ${el}`)
+//
+//   console.log(lines);
+// })
 
-  const lines = data
-  .split('\n')
-  .filter(x => x.length > 0)
-  .map((el, idx) => `${idx}: ${el}`)
-  console.log(lines);
+// try {
+//   const syncContents = fs.readFileSync(filename, 'utf8')
+//   console.log(cyan(syncContents));
+// }
+// catch (err) {
+//   console.log(red(err));
+// }
+
+fs.unlink(filename, (err) => {
+  if (err){
+    console.log(red(err));
+  } else {
+    console.log('boom deleted');
+  }
 })
