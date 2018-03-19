a1b0677b060ff211735c2cf75a93c35bcd8e674e
Kelly L <lock2846@gmail.com>
2018-03-19 14:03:40 -0600

    read directory

--- my-files.js
+++ my-files.js
@@ -1,6 +1,16 @@
-const { red, green } = require('chalk')
+const { red, green, cyan } = require('chalk')
 const path = require('path')
+const fs = require('fs')
 
-const filename = path.join(__dirname, 'test.txt')
+// const filename = path.join(__dirname, 'test.txt')
+// console.log(green(filename));
 
-console.log(green(filename));
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    console.log(red(err));
+  } else {
+  console.log(files);
+  }
+})
+
+console.log(cyan('herro'));
