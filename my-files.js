02bb9ef879f1a8e6cf157d801e1f1de689a82247
Kelly L <lock2846@gmail.com>
2018-03-19 14:58:37 -0600

    successfully appended data

--- my-files.js
+++ my-files.js
@@ -34,10 +34,43 @@ const filename = path.join(__dirname, 'test.txt')
 //   console.log(red(err));
 // }
 
-fs.unlink(filename, (err) => {
-  if (err){
-    console.log(red(err));
-  } else {
-    console.log('boom deleted');
-  }
-})
+// fs.unlink(filename, (err) => {
+//   if (err){
+//     console.log(red(err));
+//   } else {
+//     console.log('boom deleted');
+//   }
+// })
+
+function addFruit(type, count, filename='things.csv') {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.log(red(err));
+    }
+
+    else {
+      const lines = data
+        .split('\n')
+        .filter(x => x.length > 0) // takes out extra string at end
+        .map(x => x.split(',')) // makes arr of arrs
+
+      const fruits = {}
+      lines.forEach(x => fruits[x[0]] = x[1]) // puts it into an obj
+      fruits[type] = count
+
+      const newLines = []
+      Object.keys(fruits).forEach((key) => {
+        newLines.push(`${key},${fruits[key]}`)
+      })
+
+      const writeMe = newLines.join('\n') //appended new data to csv file
+      fs.writeFile(filename, writeMe, 'utf8', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    }
+  })
+}
+
+addFruit('Apple', 10)
